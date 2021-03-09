590e002018d44373f06a9bc014ed38b94c93c0ca
MatveiVasilenko <mat.vasilenkoo@gmail.com>
2021-03-10 00:10:01 +0200

    lesson 8

---
+++ database/migrations/2021_01_04_205544_create_news_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_general_ci';
+
+
+            $table->id();
+            
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('text');
+            $table->string('img');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
